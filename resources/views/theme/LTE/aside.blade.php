f12f4a75710b7e7ab9d94819c0382b03c17c2937
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2019-06-25 11:47:47 -0400

    Solución repetición migraciones roles y role_user.

--- resources/views/theme/LTE/aside.blade.php
+++ resources/views/theme/LTE/aside.blade.php
@@ -12,95 +12,95 @@
           <p>del Maule</p>
         </div>
       </div>
-      
-      <!-- ADMINISTRADOR -->
-      @if(auth()->user()->hasRole('admin'))
-      <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">ADMINISTRADOR</li>
-        <li><a href="#"><i class="fa fa-book"></i> <span>Opción</span></a></li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-pie-chart"></i>
-            <span>Opciones agrupadas</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-circle-o"></i> sub op1</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> sub op2</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> sub op3</a></li>
-          </ul>
-        </li>
-      </ul>
-      @endif
+      @if(Auth::check())
+        <!-- ADMINISTRADOR -->
+        @if(auth()->user()->hasRole('admin'))
+        <ul class="sidebar-menu" data-widget="tree">
+          <li class="header">ADMINISTRADOR</li>
+          <li><a href="#"><i class="fa fa-book"></i> <span>Opción</span></a></li>
+          <li class="treeview">
+            <a href="#">
+              <i class="fa fa-pie-chart"></i>
+              <span>Opciones agrupadas</span>
+              <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+            </a>
+            <ul class="treeview-menu">
+              <li><a href="#"><i class="fa fa-circle-o"></i> sub op1</a></li>
+              <li><a href="#"><i class="fa fa-circle-o"></i> sub op2</a></li>
+              <li><a href="#"><i class="fa fa-circle-o"></i> sub op3</a></li>
+            </ul>
+          </li>
+        </ul>
+        @endif
 
-      <!-- PROFESOR -->
-      @if(auth()->user()->hasRole('profesor'))
-      <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">PROFESOR</li>
-        <li><a href="#"><i class="fa fa-book"></i> <span>Opción</span></a></li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-pie-chart"></i>
-            <span>Opciones agrupadas</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-circle-o"></i> sub op1</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> sub op2</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> sub op3</a></li>
-          </ul>
-        </li>
-      </ul>
-      @endif
+        <!-- PROFESOR -->
+        @if(auth()->user()->hasRole('profesor'))
+        <ul class="sidebar-menu" data-widget="tree">
+          <li class="header">PROFESOR</li>
+          <li><a href="#"><i class="fa fa-book"></i> <span>Opción</span></a></li>
+          <li class="treeview">
+            <a href="#">
+              <i class="fa fa-pie-chart"></i>
+              <span>Opciones agrupadas</span>
+              <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+            </a>
+            <ul class="treeview-menu">
+              <li><a href="#"><i class="fa fa-circle-o"></i> sub op1</a></li>
+              <li><a href="#"><i class="fa fa-circle-o"></i> sub op2</a></li>
+              <li><a href="#"><i class="fa fa-circle-o"></i> sub op3</a></li>
+            </ul>
+          </li>
+        </ul>
+        @endif
 
-      <!-- REVISOR -->
-      @if(auth()->user()->hasRole('revisor'))
-      <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">REVISOR</li>
-        <li><a href="#"><i class="fa fa-book"></i> <span>Opción</span></a></li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-pie-chart"></i>
-            <span>Opciones agrupadas</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-circle-o"></i> sub op1</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> sub op2</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> sub op3</a></li>
-          </ul>
-        </li>
-      </ul>
-      @endif
+        <!-- REVISOR -->
+        @if(auth()->user()->hasRole('revisor'))
+        <ul class="sidebar-menu" data-widget="tree">
+          <li class="header">REVISOR</li>
+          <li><a href="#"><i class="fa fa-book"></i> <span>Opción</span></a></li>
+          <li class="treeview">
+            <a href="#">
+              <i class="fa fa-pie-chart"></i>
+              <span>Opciones agrupadas</span>
+              <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+            </a>
+            <ul class="treeview-menu">
+              <li><a href="#"><i class="fa fa-circle-o"></i> sub op1</a></li>
+              <li><a href="#"><i class="fa fa-circle-o"></i> sub op2</a></li>
+              <li><a href="#"><i class="fa fa-circle-o"></i> sub op3</a></li>
+            </ul>
+          </li>
+        </ul>
+        @endif
 
-      <!-- DAC -->
-      @if(auth()->user()->hasRole('dac'))
-      <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">DAC</li>
-        <li><a href="#"><i class="fa fa-book"></i> <span>Opción</span></a></li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-pie-chart"></i>
-            <span>Opciones agrupadas</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-circle-o"></i> sub op1</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> sub op2</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> sub op3</a></li>
-          </ul>
-        </li>
-      </ul>
+        <!-- DAC -->
+        @if(auth()->user()->hasRole('dac'))
+        <ul class="sidebar-menu" data-widget="tree">
+          <li class="header">DAC</li>
+          <li><a href="#"><i class="fa fa-book"></i> <span>Opción</span></a></li>
+          <li class="treeview">
+            <a href="#">
+              <i class="fa fa-pie-chart"></i>
+              <span>Opciones agrupadas</span>
+              <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+            </a>
+            <ul class="treeview-menu">
+              <li><a href="#"><i class="fa fa-circle-o"></i> sub op1</a></li>
+              <li><a href="#"><i class="fa fa-circle-o"></i> sub op2</a></li>
+              <li><a href="#"><i class="fa fa-circle-o"></i> sub op3</a></li>
+            </ul>
+          </li>
+        </ul>
+        @endif
       @endif
-
 
     </section>
     <!-- /.sidebar -->
